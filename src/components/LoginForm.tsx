db06d6e021c07425164e59766bb12d6411851571
Quillinan <milton.quillinan1798@gmail.com>
2024-10-08 14:23:07 -0300

    style: change login buttons and add input padding

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -25,7 +25,7 @@ const LoginForm: React.FC = () => {
     const validPassword = '1234';
 
     if (email === validEmail && password === validPassword) {
-      setAuth();
+      setAuth(null);
       login('true');
     } else {
       setError('Usuário ou senha incorretos');
@@ -53,7 +53,7 @@ const LoginForm: React.FC = () => {
             required
             autoComplete='email'
             onChange={e => setEmail(e.target.value)}
-            className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            className='block w-full rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           />
         </div>
       </div>
@@ -73,17 +73,17 @@ const LoginForm: React.FC = () => {
             required
             autoComplete='current-password'
             onChange={e => setPassword(e.target.value)}
-            className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            className='block w-full rounded-md border-0 py-2 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           />
         </div>
       </div>
 
       {error && <p className='text-red-500 text-sm'>{error}</p>}
 
-      <div>
+      <div className='flex flex-col items-center space-y-4'>
         <button
           type='submit'
-          className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+          className='flex w-1/2 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-md hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
         >
           Sign in
         </button>
--- src/components/SocialLoginButtons.tsx
+++ src/components/SocialLoginButtons.tsx
@@ -1,18 +1,44 @@
+import { useMsal } from '@azure/msal-react';
+import { useAuth } from '../context/AuthContext';
+import microsoft from '../../public/microsoft.svg';
+
 const SocialLoginButtons: React.FC = () => {
+  const { instance } = useMsal();
+  const { setAuth } = useAuth();
+
+  const handleAzureLogin = () => {
+    instance
+      .loginPopup({
+        scopes: ['user.read'],
+      })
+      .then(response => {
+        const token = response.accessToken;
+        if (token) {
+          setAuth(token);
+          console.log('Autenticado com sucesso', response);
+        }
+      })
+      .catch(error => {
+        console.error('Erro de autenticação:', error);
+      });
+  };
+
   return (
-    <div className='flex flex-col space-y-4'>
+    <div className='flex flex-col items-center space-y-4'>
       <button
-        className='flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
-        onClick={() => alert('Continue with Azure')}
+        className='flex items-center w-1/2 justify-center rounded-md bg-white border border-black/20 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-md hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600'
+        onClick={handleAzureLogin}
       >
-        Login with Azure
+        <img src={microsoft} alt='microsoft icon' className='mr-4 h-5 w-5' />
+        Microsoft
       </button>
-      <button
+
+      {/* <button
         className='flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600'
         onClick={() => alert('Continue with GitHub')}
       >
         Login with GitHub
-      </button>
+      </button> */}
     </div>
   );
 };
