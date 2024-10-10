dcebfaf2ea63ec51997f8cdd911c7c60a71848dd
Quillinan <milton.quillinan1798@gmail.com>
2024-10-10 18:16:32 -0300

    style: remove reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login/LoginPage';
-import ResetStyle from './styles/ResetStyle';
 import './styles/tailwind.css';
 import HomePage from './pages/Home/HomePage';
 
 export default function App() {
   return (
     <>
-      <ResetStyle />
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<LoginPage />} />
