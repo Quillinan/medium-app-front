1e06e01dd1f1436be037f27cce8df51c0407672b
Quillinan <milton.quillinan1798@gmail.com>
2024-10-14 17:48:29 -0300

    chore: create vitest.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import 'vitest';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
-  },
 });
