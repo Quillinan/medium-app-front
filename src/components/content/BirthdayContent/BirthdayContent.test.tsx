11f5ca3783e7f0c1f3d8dc034dce775fca7a0f26
Quillinan <milton.quillinan@gmail.com>
2024-11-04 02:24:28 -0300

    test: update BirthdayContent test

--- src/components/content/BirthdayContent/BirthdayContent.test.tsx
+++ src/components/content/BirthdayContent/BirthdayContent.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import BirthdayContent from './BirthdayContent';
 import { getMonthlyBirthdays } from '@services/GetMonthlyBirthdays/GetMonthlyBirthdays';
 import { showLoading } from '@utils/LoadingHelper/LoadingHelper';
+import { Mock } from 'vitest';
 
-vi.mock('@services/GetMonthlyBirthdays/getMonthlyBirthdays', () => ({
+vi.mock('@services/GetMonthlyBirthdays/GetMonthlyBirthdays', () => ({
   getMonthlyBirthdays: vi.fn(),
 }));
 vi.mock('@utils/LoadingHelper/LoadingHelper', () => ({
@@ -18,35 +19,42 @@ describe('BirthdayContent', () => {
 
   it('should display custom error message from API response', async () => {
     const apiErrorResponse = { message: 'Erro no servidor' };
-    (getMonthlyBirthdays as jest.Mock).mockResolvedValue(apiErrorResponse);
-    (showLoading as jest.Mock).mockResolvedValue(apiErrorResponse);
+    (getMonthlyBirthdays as Mock).mockResolvedValue(apiErrorResponse);
+    (showLoading as Mock).mockResolvedValue(apiErrorResponse);
 
-    render(<BirthdayContent />);
+    await act(async () => {
+      render(<BirthdayContent />);
+    });
 
     expect(showLoading).toHaveBeenCalled();
+
     const error = await screen.findByText('Erro no servidor');
     expect(error).toBeInTheDocument();
   });
 
   it('should display error message if fetching data fails', async () => {
     const errorMessage = 'Erro ao carregar os dados da API';
-    (getMonthlyBirthdays as jest.Mock).mockRejectedValue(
-      new Error(errorMessage)
-    );
-    (showLoading as jest.Mock).mockRejectedValue(new Error(errorMessage));
 
-    render(<BirthdayContent />);
+    (getMonthlyBirthdays as Mock).mockRejectedValue(new Error(errorMessage));
+    (showLoading as Mock).mockRejectedValue(new Error(errorMessage));
+
+    await act(async () => {
+      render(<BirthdayContent />);
+    });
 
     expect(showLoading).toHaveBeenCalled();
-    const error = await screen.findByText(errorMessage);
+
+    const error = await screen.findByText('Erro ao carregar os dados da API');
     expect(error).toBeInTheDocument();
   });
 
   it('should display birthday data when fetching is successful', async () => {
-    (getMonthlyBirthdays as jest.Mock).mockResolvedValue(mockBirthdays);
-    (showLoading as jest.Mock).mockResolvedValue(mockBirthdays);
+    (getMonthlyBirthdays as Mock).mockResolvedValue(mockBirthdays);
+    (showLoading as Mock).mockResolvedValue(mockBirthdays);
 
-    render(<BirthdayContent />);
+    await act(async () => {
+      render(<BirthdayContent />);
+    });
 
     expect(showLoading).toHaveBeenCalled();
 
@@ -57,16 +65,18 @@ describe('BirthdayContent', () => {
   });
 
   it('should sort the birthdays by date', async () => {
-    (getMonthlyBirthdays as jest.Mock).mockResolvedValue(mockBirthdays);
-    (showLoading as jest.Mock).mockResolvedValue(mockBirthdays);
+    (getMonthlyBirthdays as Mock).mockResolvedValue(mockBirthdays);
+    (showLoading as Mock).mockResolvedValue(mockBirthdays);
 
-    render(<BirthdayContent />);
+    await act(async () => {
+      render(<BirthdayContent />);
+    });
 
     expect(showLoading).toHaveBeenCalled();
 
     await waitFor(() => {
-      const aliceName = screen.getByTestId('name-Alice');
-      const bobName = screen.getByTestId('name-Bob');
+      const aliceName = screen.getByText('Alice');
+      const bobName = screen.getByText('Bob');
 
       expect(aliceName).toBeInTheDocument();
       expect(bobName).toBeInTheDocument();
@@ -76,8 +86,10 @@ describe('BirthdayContent', () => {
     });
   });
 
-  it('should match snapshot', () => {
-    const { asFragment } = render(<BirthdayContent />);
-    expect(asFragment()).toMatchSnapshot();
+  it('should match snapshot', async () => {
+    await act(async () => {
+      const { asFragment } = render(<BirthdayContent />);
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
