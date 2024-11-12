193714cafc3602709adb56b1e43ebdac32faa55d
Quillinan <milton.quillinan1798@gmail.com>
2024-11-12 17:21:56 -0300

    test: update TableBirthday test and snapshots

--- src/components/TableBirthday/TableBirthday.test.tsx
+++ src/components/TableBirthday/TableBirthday.test.tsx
@@ -4,38 +4,33 @@ import { render, screen } from '@testing-library/react';
 
 describe('TableBirthday', () => {
   const mockData: TableBirthdayProps['data'] = [
-    { NOME: 'João', DTNASCIMENTO: 1 },
-    { NOME: 'Maria', DTNASCIMENTO: 17 },
+    { NOME: 'João', DTNASCIMENTO: 1, DESCRICAO: 'Financeiro', IMAGEM: '' },
+    { NOME: 'Maria', DTNASCIMENTO: 17, DESCRICAO: 'RH', IMAGEM: '' },
   ];
+
   it('should render a message when no data is provided', () => {
     render(<TableBirthday data={[]} />);
 
     const noDataMessage = screen.getByText(
       /Nenhum aniversariante encontrado./i
     );
-    expect(noDataMessage).toBeInTheDocument;
-  });
-  it('should render the table header correctly', () => {
-    render(<TableBirthday data={[]} />);
-
-    const nomeHeader = screen.getByText(/Nome/i);
-    const diaHeader = screen.getByText(/Dia/i);
-
-    expect(nomeHeader).toBeInTheDocument;
-    expect(diaHeader).toBeInTheDocument;
+    expect(noDataMessage).toBeInTheDocument();
   });
 
-  it('should render the data rows when data is provided', () => {
+  it('should render the data cards when data is provided', () => {
     render(<TableBirthday data={mockData} />);
 
-    expect(screen.getByText('João')).toBeInTheDocument;
-    expect(screen.getByText('Maria')).toBeInTheDocument;
-    expect(screen.getByText('1')).toBeInTheDocument;
-    expect(screen.getByText('17')).toBeInTheDocument;
+    expect(screen.getByText('João')).toBeInTheDocument();
+    expect(screen.getByText('Maria')).toBeInTheDocument();
+    expect(screen.getByText('Aniversário: 1')).toBeInTheDocument();
+    expect(screen.getByText('Aniversário: 17')).toBeInTheDocument();
+
+    expect(screen.getByText('Setor: Financeiro')).toBeInTheDocument();
+    expect(screen.getByText('Setor: RH')).toBeInTheDocument();
   });
 
   it('should match snapshot', () => {
-    const { asFragment } = render(<TableBirthday data={[]} />);
+    const { asFragment } = render(<TableBirthday data={mockData} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
