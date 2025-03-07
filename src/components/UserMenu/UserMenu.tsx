d8169821a70aa6c866df92adb7b0435a49c3014b
Quillinan <milton.quillinan@gmail.com>
2025-03-07 16:47:26 -0300

    feat: remove user icon and add user name with logout icon

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,10 +1,19 @@
 import { useAuth } from '@auth/useAuth';
 import IconButton from '@components/buttons/IconButton/IconButton';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { FiLogOut } from 'react-icons/fi'; // Importa o ícone de logout
 
 const UserMenu: React.FC = () => {
   const { removeAuth } = useAuth();
   const navigate = useNavigate();
+  const [userName, setUserName] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Obtém o nome do usuário do sessionStorage
+    const storedName = sessionStorage.getItem('name');
+    setUserName(storedName);
+  }, []);
 
   const logout = () => {
     removeAuth();
@@ -14,15 +23,14 @@ const UserMenu: React.FC = () => {
   return (
     <div className='hidden md:block'>
       <div className='ml-4 flex items-center md:ml-6'>
+        {userName ? (
+          <span className='mr-4 text-gray-300 font-medium'>{userName}</span>
+        ) : (
+          <span className='mr-4 text-gray-300 italic'>Usuário</span>
+        )}
         <IconButton
           onClick={logout}
-          icon={
-            <img
-              className='h-8 w-8 rounded-full'
-              src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-              alt='User'
-            />
-          }
+          icon={<FiLogOut className='text-xl text-gray-300' />}
         />
       </div>
     </div>
